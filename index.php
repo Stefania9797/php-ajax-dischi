65922984f2d4d612d0e33ce2d5c40b6bad3879e7
Stefania9797 <stefaniamastrel@gmail.com>
2021-06-24 14:53:18 +0200

    complete ms 1

--- index.php
+++ index.php
@@ -16,16 +16,21 @@ require __DIR__ . '/database/db.php';
         <img src="./dist/img/logo-small.svg" alt="">
     </header>
     <div class="container">
+    <?php foreach ($albums as $album) { ?>
         <div class="card">
             <div class="cover">
-                
+            <img src="<?= $album["poster"]; ?>" alt="">
             </div>
             <div class="info">
-                <h3>{{album.title}}</h3>
-                <h5>{{album.author}}</h5>
-                <h5>{{album.year}}</h5>
+                    
+                <h3><?= $album["title"] ?></h3>
+                <h5><?= $album["author"] ?></h5>
+                <h5><?= $album["year"] ?></h5>
             </div>
         </div>
+                <?php
+                } 
+            ?>
     </div>
 </body>
 </html>
