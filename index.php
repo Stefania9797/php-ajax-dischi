9a4340e6ca1cf934bdce77f5838bf3070c068ce1
Stefania9797 <stefaniamastrel@gmail.com>
2021-06-24 14:35:58 +0200

    create database

--- index.php
+++ index.php
@@ -1,10 +1,15 @@
+<?php 
+require __DIR__ . '/database/db.php';
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Ajax Dischi</title>
+    <link rel="stylesheet" href="./dist/css/app.css">
 </head>
 <body>
     
