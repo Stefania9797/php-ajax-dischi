dee5626ee7f8ffcf5637c2f9718b418d57da519b
Stefania9797 <stefaniamastrel@gmail.com>
2021-06-24 15:29:50 +0200

    init ms 2

--- index.php
+++ index.php
@@ -1,16 +1,8 @@
 <?php 
 require __DIR__ . '/database/db.php';
+require __DIR__ . '/templates/head.php';
 ?>
 
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Ajax Dischi</title>
-    <link rel="stylesheet" href="./dist/css/app.css">
-</head>
 <body>
     <header>
         <img src="./dist/img/logo-small.svg" alt="">
